f196f3494e6ebd29658b9e7d88bcc0361ed9168b
Jo-cloud85 <joan_tta@live.com.sg>
2024-05-22 18:00:19 +0800

    1st commit - day38workshop

---
+++ ibf-csf-day38/day38workshop/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CitiesListComponent } from './cities-list/cities-list.component';
+import { WeatherDetailsComponent } from './weather-details/weather-details.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CitiesListComponent,
+    WeatherDetailsComponent,
+    ErrorPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
