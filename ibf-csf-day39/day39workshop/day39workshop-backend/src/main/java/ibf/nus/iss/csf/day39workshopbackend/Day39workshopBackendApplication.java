8e86fa554bc0c5f386f16c1218977cbcc581e205
Jo-cloud85 <joan_tta@live.com.sg>
2024-05-23 09:22:53 +0800

    1st commit - day39workshop

---
+++ ibf-csf-day39/day39workshop/day39workshop-backend/src/main/java/ibf/nus/iss/csf/day39workshopbackend/Day39workshopBackendApplication.java
@@ -0,0 +1,13 @@
+package ibf.nus.iss.csf.day39workshopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day39workshopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day39workshopBackendApplication.class, args);
+	}
+
+}
---
+++ ibf-csf-day39/day39workshop/day39workshop-backend/src/test/java/ibf/nus/iss/csf/day39workshopbackend/Day39workshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ibf.nus.iss.csf.day39workshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day39workshopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
