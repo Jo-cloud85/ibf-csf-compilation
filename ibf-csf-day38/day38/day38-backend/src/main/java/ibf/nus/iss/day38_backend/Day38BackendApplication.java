af2f6be87a331e92110f47b0375d678f90ee9e88
Jo-cloud85 <joan_tta@live.com.sg>
2024-06-05 09:13:06 +0800

    1st commit - day38

---
+++ ibf-csf-day38/day38/day38-backend/src/main/java/ibf/nus/iss/day38_backend/Day38BackendApplication.java
@@ -0,0 +1,13 @@
+package ibf.nus.iss.day38_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day38BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day38BackendApplication.class, args);
+	}
+
+}
