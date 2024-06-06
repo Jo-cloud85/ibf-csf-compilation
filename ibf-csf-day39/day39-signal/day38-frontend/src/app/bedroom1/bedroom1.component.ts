3358aa78c825de189f6afacdb9af1d25a0479cac
Jo-cloud85 <joan_tta@live.com.sg>
2024-06-06 09:25:49 +0800

    1st commit - day39-signal

---
+++ ibf-csf-day39/day39-signal/day38-frontend/src/app/bedroom1/bedroom1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bedroom1',
+  templateUrl: './bedroom1.component.html',
+  styleUrl: './bedroom1.component.css'
+})
+export class Bedroom1Component {
+
+}
